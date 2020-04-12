3d5bad62ee2b5dbaf57cc2405f29c386c5f2bd1b
Andrew Lapin <andreylapin95@gmail.com>
2020-04-12 16:52:52 +0300

    Add author of message

--- src/main/java/com/lapin/bodymechanic/domain/Message.java
+++ src/main/java/com/lapin/bodymechanic/domain/Message.java
@@ -2,11 +2,9 @@ package com.lapin.bodymechanic.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -15,17 +13,30 @@ public class Message {
     @Setter
     @Getter
     private Long id;
+
     @Setter
     @Getter
     private String text;
+
     @Setter
     @Getter
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    @Setter
+    @Getter
+    private User author;
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
     }
